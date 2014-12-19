1e6af92d478cc7a24b27e0735a42f369d2f6c4dc
Will Dickson <will@iorodeo.com>
2014-12-19 08:24:40 -0800

    In corpoerated Tim's changes which 1. added ACV test case in exportToFile in Result class. 2. added a simple writer.write(ident); line to the end of exportToFile to add information about test parameters. 3.Added a newline the end of each of the String.format lines in updateGraph() for each test case to keep the output file data clean.
    Also, reformated all .java files (replaced tabs with spaces.)

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -12,24 +12,24 @@ import javax.swing.border.EtchedBorder;
 
 
 public class MyGraph extends JPanel {
-	Result result;
-	
-	public MyGraph(Result result)
-	{
-		this.result = result;
-	}
-	
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
+    Result result;
+
+    public MyGraph(Result result)
+    {
+        this.result = result;
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g2);  
         //g2.setStroke(new BasicStroke(1));
-		//g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        //g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         setBackground(Color.WHITE);
         setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
         result.paintComponent(g);
-        
-       
-   }
+
+
+    }
 
 
 }
